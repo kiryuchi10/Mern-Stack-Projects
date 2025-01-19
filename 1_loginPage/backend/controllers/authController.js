cea72504b9cd8ee3dd1c46675f5febc15cf94ba9
kiryuchi10 <kaminer_sol2@naver.com>
2025-01-20 01:17:00 +0900

    second commit

---
+++ 1_loginPage/backend/controllers/authController.js
@@ -0,0 +1,35 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.signup = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = new User({ email, password });
+        await user.save();
+        res.status(201).send('User registered successfully');
+    } catch (err) {
+        res.status(400).send('Error registering user');
+    }
+};
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) return res.status(404).send('User not found');
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).send('Invalid credentials');
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ token });
+    } catch (err) {
+        res.status(500).send('Server error');
+    }
+};
+
+exports.forgotPassword = async (req, res) => {
+    // Implement forgot password functionality here
+    res.send('Forgot password functionality is pending');
+};
